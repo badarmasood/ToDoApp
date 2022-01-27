6fa8364785d8a0d9e14b955e0d7069e499f8cd05
Badar Masood <badarmasood99@gmail.com>
2022-01-27 23:51:32 +0500

    ReactTodoApp

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ function App() {
 
   const [list, setlist] = useState([]);
 
-  const [item, setitem] = useState();
+  const [item, setitem] = useState("");
 
   const Additem = () => {
     setlist((olditems) => {
       return [...olditems, item];
     });
+    setitem("");
   };
   const itemEvent = (e) => {
     setitem(e.target.value);
@@ -25,12 +26,22 @@ function App() {
           <br />
           <h1> To Do List</h1>
           <br />
-          <input type="text" placeholder="add an Item" onChange={itemEvent} />
-          <button onClick={Additem}> + </button>
+          <input
+            type="text"
+            placeholder="add an Item"
+            value={item}
+            onChange={itemEvent}
+          />
+          <button onClick={Additem}>+</button>
 
           <ol>
             {list.map((items) => {
-              return <li>{items}</li>;
+              return (
+                <div className="todo_style">
+                  <span className="fa-times"> X </span>
+                  <li>{items}</li>
+                </div>
+              );
             })}
           </ol>
         </div>
