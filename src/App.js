2f5012cfbb8270a021fcc9e868339dbc384b9ee2
Badar Masood <badarmasood99@gmail.com>
2022-01-27 22:44:59 +0500

    ReactTodoApp

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import react from "react";
+import react, { useState } from "react";
 import reactDom from "react-dom";
 import "./index.css";
 
 function App() {
+  //const [list, setlist] = useState(["banana", "mango", "apple"]);
+
+  const [list, setlist] = useState([]);
+
+  const [item, setitem] = useState();
+
+  const Additem = () => {
+    setlist((olditems) => {
+      return [...olditems, item];
+    });
+  };
+  const itemEvent = (e) => {
+    setitem(e.target.value);
+  };
+
   return (
     <>
       <div className="main_div">
         <div className="center_div">
           <br />
-          <h1> TO Do List</h1>
+          <h1> To Do List</h1>
           <br />
-          <input type="text" placeholder="add a Items" />
-          <button> + </button>
+          <input type="text" placeholder="add an Item" onChange={itemEvent} />
+          <button onClick={Additem}> + </button>
 
           <ol>
-            <li>Buy mango</li>
+            {list.map((items) => {
+              return <li>{items}</li>;
+            })}
           </ol>
         </div>
       </div>
