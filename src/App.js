f29bce6bd76ed96c4f5d781e8fb8e2b35321d74c
Badar Masood <badarmasood99@gmail.com>
2022-01-27 18:37:41 +0500

    ReactTodoApp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react";
+import reactDom from "react-dom";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1> TO Do List</h1>
+          <br />
+          <input type="text" placeholder="add a Items" />
+          <button> + </button>
+
+          <ol>
+            <li>Buy mango</li>
+          </ol>
+        </div>
+      </div>
+    </>
   );
 }
 
